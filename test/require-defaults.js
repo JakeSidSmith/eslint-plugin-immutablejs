8fd1221fe421cb701252fdea46a8fb9f88c80c95
Jake 'Sid' Smith <jake@dabapps.com>
2018-07-26 15:36:08 +0100

    Test require-defaults

---
+++ test/require-defaults.js
@@ -0,0 +1,47 @@
+import test from 'ava';
+import rule from '../lib/rules/require-defaults';
+import RuleTester from 'eslint/lib/testers/rule-tester';
+import { constructValidSnippets, constructInvalidSnippets } from './_construct-snippets';
+
+const parserOptions = {
+    ecmaVersion: 6,
+    sourceType: 'module'
+};
+
+const errors = [
+    {
+        message: 'ImmutableJS getters must have a default value.'
+    }
+];
+
+test('"require-defaults" Rule', t => {
+    const ruleTester = new RuleTester();
+
+    const valid = constructValidSnippets([
+        `
+        a.get('foo', 'bar');
+        `,
+        `
+        a.getIn('foo', 'bar');
+        `,
+        `
+        a.getIn(['foo', 'bar'], 'baz');
+        `
+    ], parserOptions);
+
+    const invalid = constructInvalidSnippets([
+        `
+        a.get('foo');
+        `,
+        `
+        a.getIn('foo');
+        `,
+        `
+        a.getIn(['foo', 'bar']);
+        `
+    ], parserOptions, errors);
+
+    ruleTester.run('immutable-no-map-set', rule, { valid, invalid });
+
+    t.pass();
+});
